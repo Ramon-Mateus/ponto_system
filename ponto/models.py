8aec87c48e3951422dee3eee31add86f424aaa42
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2023-08-29 16:46:04 -0300

    start models

--- ponto/models.py
+++ ponto/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Hours(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start = models.DecimalField()
+    end = models.DecimalField()
+    total = models.IntegerField()
+    data = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.total
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
